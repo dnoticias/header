bcbfcaea608f3f54ad57ec55e86442ed15494180
Pedro Mendes <pedro.trabalho.uma@gmail.com>
2020-03-26 20:59:06 +0000

    update headerIndex element

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js',
-    library: 'Header',
+    library: 'HeaderIndex',
     libraryTarget: 'umd'
   }
 };
